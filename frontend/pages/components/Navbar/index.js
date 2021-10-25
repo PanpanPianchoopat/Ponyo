fe2095f7d5922e82cfa6e2de1daf45218d59c617
PanpanPianchoopat <pploypapass@gmail.com>
2021-10-25 22:29:36 +0700

    Create navbar component

---
+++ frontend/pages/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledNav } from "./styled";
+
+const Navbar = () => {
+  return (
+    <StyledNav>
+      <p>1</p>
+      <p>2</p>
+    </StyledNav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/pages/components/Navbar/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #2e3840;
+  display: flex;
+  color: white;
+`;
