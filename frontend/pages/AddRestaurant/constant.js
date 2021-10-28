b331443b43c02a078c56f82ec7a2b7d8a5830879
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-10-28 15:53:21 +0700

    AddRestaurant Form (#2)
    * Create AddRestaurant page
    
    * Modify AddRestaurant form

---
+++ frontend/pages/AddRestaurant/constant.js
@@ -0,0 +1,21 @@
+export const REST_TYPE = [
+  "Casual Dining",
+  "Food Trucks",
+  "Fast Food",
+  "Café",
+  "Family Style",
+  "Pub",
+  "Buffet",
+];
+
+export const MAX_IMAGE = 5;
+
+export const DAYS_OF_WEEK = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Firday",
+  "Saturday",
+  "Sunday",
+];
