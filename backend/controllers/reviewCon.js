e1f44af30e42b768ae59226be940abf964f4d41c
pungjung10 <pungjungg@gmail.com>
2021-10-31 20:34:26 +0700

    Create: GetAllReviews

--- backend/controllers/reviewCon.js
+++ backend/controllers/reviewCon.js
@@ -4,7 +4,7 @@ import Review from "../models/reviewModel.js";
 const router = express.router;
 
 export const addReview = async (req, res) => {
-  res.send("Add Review API");
+  
   const {rest_id} = req.params;
   const{
     reviewer,
@@ -26,10 +26,22 @@ export const addReview = async (req, res) => {
 
   try {
     await newReview.save();
+    res.send("Add Review API");
     res.status(201).json(newReview);
   } catch (error) {
     res.status(404).json({ Error: error.message });
   }
 };
 
-
+export const getAllReview = async (req, res) => {
+  const {rest_id} = req.params;
+  try {
+    const Reviews = await Review.find({
+      rest_id: rest_id
+    }
+    );
+    res.status(200).json(Reviews);
+  } catch (error) {
+    res.status(404).json({ Error: error.message });
+  }
+};
--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addReview } from "../controllers/reviewCon.js";
+import { addReview,getAllReview } from "../controllers/reviewCon.js";
 
 const router = express.Router();
 
 router.post("/:rest_id", addReview);
+router.get("/:rest_id", getAllReview);
 
 export default router;
