c35eda259ee7b9902d753108e9eabff9ada8e6fc
qbeamm <qbeamm@gmail.com>
2021-11-18 02:50:28 +0700

    Trending page

---
+++ frontend/pages/trending/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import COLORS from "../../public/constant/colors";
+
+export const Header = styled.div`
+  padding: 50px 0 100px 0;
+  background: ${COLORS.PRIMARY_DARK};
+`;
+
+export const Type = styled.div`
+  padding: 30px 0 0 0;
+`;
