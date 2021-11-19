2fc185a4ff48a6c6dd7ed4375cbb983d45124545
qbeamm <qbeamm@gmail.com>
2021-11-20 03:14:03 +0700

    Edit select option

--- frontend/pages/search/index.js
+++ frontend/pages/search/index.js
@@ -7,6 +7,12 @@ import Card from "../components/Card";
 import { SAMPLE_DATA } from "../components/Card/constant";
 import { BackTop } from "antd";
 import Category from "./components/Category";
+import {
+  STATUS_OPTION,
+  FILTER_OPTION,
+  PRICE_OPTION,
+  CUISINE_OPTION,
+} from "./constant";
 import RestaurantAPI from "../api/restaurantAPI";
 import {
   Container,
@@ -28,14 +34,26 @@ import {
 const SearchRestaurant = () => {
   const { Option } = Selecter;
   const statusOption = ["ALL", "OPEN", "CLOSE"];
+  const filterOption = ["Name", "Address"];
+  const priceOption = ["Price", "0-500", "500-1000", "1000-5000", "5000-10000"];
+  const cuisineOption = [
+    "Cuisine",
+    "Casual Dining",
+    "Food Trucks",
+    "Fast-food",
+    "Café",
+    "Family Style",
+    "Pub",
+    "Buffet",
+  ];
   const [status, setStatus] = useState("ALL");
   const [getRestaurants, setRestaurants] = useState(null);
 
   const [searchValue, setSearchValue] = useState({
-    nameAd: "name",
+    filter: "Name",
     input: "null",
-    price: "price",
-    cuisine: "cuisine",
+    price: "0",
+    cuisine: "Cuisine",
   });
 
   useEffect(() => {
@@ -86,16 +104,17 @@ const SearchRestaurant = () => {
               <Selecter
                 bordered={false}
                 size="large"
-                defaultValue="name"
+                defaultValue="Name"
                 dropdownStyle={{
                   backgroundColor: COLORS.PRIMARY_LIGHT,
                 }}
                 onChange={(e) => {
-                  setSearchValue({ ...searchValue, nameAd: e });
+                  setSearchValue({ ...searchValue, filter: e });
                 }}
               >
-                <Option value="name">Name</Option>
-                <Option value="address">address</Option>
+                {filterOption.map((type) => {
+                  return <Option value={type}>{type}</Option>;
+                })}
               </Selecter>
               <Search
                 bordered={false}
@@ -108,7 +127,7 @@ const SearchRestaurant = () => {
               <Selecter
                 bordered={false}
                 size="large"
-                defaultValue="0"
+                defaultValue="Price"
                 dropdownStyle={{
                   backgroundColor: COLORS.PRIMARY_LIGHT,
                 }}
@@ -116,16 +135,14 @@ const SearchRestaurant = () => {
                   setSearchValue({ ...searchValue, price: e });
                 }}
               >
-                <Option value="0">Price</Option>
-                <Option value="1">0-500</Option>
-                <Option value="2">500-1,000</Option>
-                <Option value="3">1,000-5,000</Option>
-                <Option value="4">5,000-10,000</Option>
+                {priceOption.map((type, index) => {
+                  return <Option value={index.toString()}>{type}</Option>;
+                })}
               </Selecter>
               <Selecter
                 bordered={false}
                 size="large"
-                defaultValue="null"
+                defaultValue="Cuisine"
                 dropdownStyle={{
                   backgroundColor: COLORS.PRIMARY_LIGHT,
                 }}
@@ -133,14 +150,9 @@ const SearchRestaurant = () => {
                   setSearchValue({ ...searchValue, cuisine: e });
                 }}
               >
-                <Option value="null">Cuisine</Option>
-                <Option value="Casual Dining">Casual Dining</Option>
-                <Option value="Food Trucks">Food Trucks</Option>
-                <Option value="Fast-food">Fast-food</Option>
-                <Option value="Café">Café</Option>
-                <Option value="Family Style">Family Style</Option>
-                <Option value="Pub">Pub</Option>
-                <Option value="Buffet">Buffet</Option>
+                {cuisineOption.map((type) => {
+                  return <Option value={type}>{type}</Option>;
+                })}
               </Selecter>
               <StyleButton>SEARCH</StyleButton>
             </Search.Group>
