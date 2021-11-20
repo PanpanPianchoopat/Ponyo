5a62d7f48d55208f56d9eced6095c01a20db8901
PanpanPianchoopat <pploypapass@gmail.com>
2021-11-20 12:18:04 +0700

    Link navbar to pages

--- frontend/pages/components/Navbar/index.js
+++ frontend/pages/components/Navbar/index.js
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 import { StyledNav, Logo, MenuItem, StyledImage, Menu } from "./styled";
 import MenuButton from "./components/MenuButton";
 import { SEARCH, TREND } from "./constant";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const [selected, setSelcted] = useState(SEARCH);
+  const router = useRouter();
+
+  const handleClick = (menu) => {
+    if (menu == SEARCH) {
+      setSelcted(SEARCH);
+      router.push("/search");
+    } else if (menu == TREND) {
+      setSelcted(TREND);
+      router.push("/trending");
+    }
+  };
 
   return (
     <StyledNav>
@@ -13,12 +25,15 @@ const Navbar = () => {
       </Logo>
       <Menu>
         <MenuItem
-          onClick={() => setSelcted(SEARCH)}
+          onClick={() => handleClick(SEARCH)}
           active={selected === SEARCH}
         >
           Search Restaurant
         </MenuItem>
-        <MenuItem onClick={() => setSelcted(TREND)} active={selected === TREND}>
+        <MenuItem
+          onClick={() => handleClick(TREND)}
+          active={selected === TREND}
+        >
           Discover Trending
         </MenuItem>
         <MenuButton />
