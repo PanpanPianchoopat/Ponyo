6513090d705bee2649f4d16654bb9d95d27d3f8a
PanpanPianchoopat <pploypapass@gmail.com>
2021-12-04 15:49:34 +0700

    Comment Lock and Navbar

--- frontend/components/Lock/index.js
+++ frontend/components/Lock/index.js
@@ -1,3 +1,9 @@
+/*******************************************************************************
+ * Lock component - lock some section for non-login users
+ * 'type' specifies type of lock. It can be REVIEW and TREND constant or other
+ *        values. It is used to determine lock message.
+ ******************************************************************************/
+
 import React from "react";
 import { LockContainer, LockIcon, LockText } from "./styled";
 import Button from "../Button";
@@ -5,16 +11,22 @@ import Router from "next/router";
 import { REVIEW, TREND } from "./constant";
 
 const Lock = (props) => {
-  const messege =
+  const reviewLockText = "write review";
+  const trendingLockText = "view trending restaurants";
+  const defaultLockText = "proceed";
+
+  /* Set lock message according to lock type */
+  const message =
     props.type == REVIEW
-      ? "write review"
+      ? reviewLockText
       : props.type == TREND
-      ? "view trending restaurants"
-      : "proceed";
+      ? trendingLockText
+      : defaultLockText;
+
   return (
     <LockContainer>
       <LockIcon src="/assets/lock.svg" />
-      <LockText>Please login to {messege}</LockText>
+      <LockText>Please login to {message}</LockText>
       <Button
         variant="red"
         outline="round"
